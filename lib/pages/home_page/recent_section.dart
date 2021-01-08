42bf886db8765e6d4fe8d9cf1abe2cda1ca2bca1
Kraisuwan <ykraisuwan@eng.psu.ac.th>
2021-01-08 16:33:04 +0700

    sdfdf

--- lib/pages/home_page/recent_section.dart
+++ lib/pages/home_page/recent_section.dart
@@ -18,13 +18,29 @@ class SectionRecent extends StatelessWidget {
           ),
           SizedBox(height: 20.0),
           Container(
+            height: 200,
             child: ListView.builder(
-              scrollDirection: Axis.vertical,
+              scrollDirection: Axis.horizontal,
               itemCount: feeds.length,
               itemBuilder: (BuildContext context, int index) {
+                final recent = recents[index];
+
                 return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 6.0),
-                  child: Text(recents[index].name),
+                  padding: EdgeInsets.only(right: 10),
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          recent.introImage,
+                          fit: BoxFit.fitHeight,
+                          height: 150,
+                        ),
+                        SizedBox(height: 10),
+                        Text(recent.name, style: TextStyle(fontSize: 16.0)),
+                      ],
+                    ),
+                  ),
                 );
               },
             ),
